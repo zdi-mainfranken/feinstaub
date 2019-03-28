1db6000a8da3d5b1de5a334c3c88c0d628c11580
Philipp Zedler <philipp@neue-musik.com>
2019-03-28 17:41:56 +0100

    quickfix

--- AQI/aqi.py
+++ AQI/aqi.py
@@ -101,8 +101,10 @@ if __name__ == "__main__":
         cmd_set_mode(1);
         for t in range(15):
             values = cmd_query_data();
-            #if values is not None:
-            print("PM2.5: ", values[0], ", PM10: ", values[1])
+            try:
+                print("PM2.5: ", values[0], ", PM10: ", values[1])
+            except IndexError:
+                print("A strange error occured.")
             time.sleep(2)
 
         # open stored data
